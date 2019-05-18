eca4935715860d3596f008cd21cd0214453c0d93
Justine Berretta <justberretta@gmail.com>
2019-05-18 02:15:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-  if int % 3 == 0 
-    puts "Fizz"
+def fizzbuzz(int)
+  if int % 15 == 0 
+    puts "Fizzbuzz"
   elsif int % 5 == 0 
     puts "Buzz"
-  elsif int % 15 == 0 
-    puts "Fizzbuzz"
+  elsif int % 3 == 0 
+    puts "Fizz"
   else
     puts "nil"
+  end
 end
